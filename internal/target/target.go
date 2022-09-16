34c2e99a287a490a228d85b3c0f8fa297f62b878
CHIKAMATSU Naohiro <n.chika156@gmail.com>
2022-09-16 23:25:12 +0900

    Add code to generate unit tests for print package

--- internal/target/target.go
+++ internal/target/target.go
@@ -70,6 +70,8 @@ func Files(name, importPath string, lib, cli, noRoot bool) map[string]string {
 		files[path] = code
 		path, code = printFile(name, importPath, noRoot)
 		files[path] = code
+		path, code = printTestFile(name, noRoot)
+		files[path] = code
 	}
 
 	path, code := makefile(name, importPath, lib, cli, noRoot)
@@ -895,7 +897,8 @@ func printFile(name, importPath string, noRoot bool) (string, string) {
 	} else {
 		path = filepath.Join(name, "internal", "print", "print.go")
 	}
-	data := `package print
+	data := `// Package print defines functions to accept colored standard output and user input
+package print
 
 import (
 	"fmt"
@@ -914,30 +917,36 @@ var (
 	Stderr = colorable.NewColorableStderr()
 )
 
-// Info print information message at STDOUT.
+// Info print information message at STDOUT in green.
+// This function is used to print some information (that is not error) to the user.
 func Info(msg string) {
-	fmt.Fprintf(Stdout, "%s:%s: %s\n",
-		cmdinfo.Name, color.GreenString("INFO "), msg)
+	fmt.Fprintf(Stdout, "%s: %s\n", color.GreenString("INFO "), msg)
 }
 
-// Warn print warning message at STDERR.
+// Warn print warning message at STDERR in yellow.
+// This function is used to print warning message to the user.
 func Warn(err interface{}) {
-	fmt.Fprintf(Stderr, "%s:%s: %v\n",
-		cmdinfo.Name, color.YellowString("WARN "), err)
+	fmt.Fprintf(Stderr, "%s: %v\n", color.YellowString("WARN "), err)
 }
 
-// Err print error message at STDERR.
+// Err print error message at STDERR in yellow.
+// This function is used to print error message to the user.
 func Err(err interface{}) {
-	fmt.Fprintf(Stderr, "%s:%s: %v\n",
-		cmdinfo.Name, color.HiYellowString("ERROR"), err)
+	fmt.Fprintf(Stderr, "%s: %v\n", color.HiYellowString("ERROR"), err)
 }
 
-// Fatal print dying message at STDERR.
+// OsExit is wrapper for  os.Exit(). It's for unit test.
+var OsExit = os.Exit
+
+// Fatal print dying message at STDERR in red.
+// After print message, process will exit
 func Fatal(err interface{}) {
-	fmt.Fprintf(Stderr, "%s:%s: %v\n",
-		cmdinfo.Name, color.RedString("FATAL"), err)
-	os.Exit(1)
+	fmt.Fprintf(Stderr, "%s: %v\n", color.RedString("FATAL"), err)
+	OsExit(1)
 }
+
+// FmtScanln is wrapper for fmt.Scanln(). It's for unit test.
+var FmtScanln = fmt.Scanln
 
 // Question displays the question in the terminal and receives an answer from the user.
 func Question(ask string) bool {
@@ -945,7 +954,7 @@ func Question(ask string) bool {
 
 	fmt.Fprintf(Stdout, "%s:%s: %s",
 		cmdinfo.Name, color.GreenString("CHECK"), ask+" [Y/n] ")
-	_, err := fmt.Scanln(&response)
+	_, err := FmtScanln(&response)
 	if err != nil {
 		// If user input only enter.
 		if strings.Contains(err.Error(), "expected newline") {
@@ -963,10 +972,358 @@ func Question(ask string) bool {
 	default:
 		return Question(ask)
 	}
-}	
+}
 `
-
 	data = strings.Replace(data, "XXX_PATH_XXX", importPath, -1)
+	return path, data
+}
+
+func printTestFile(name string, noRoot bool) (string, string) {
+	var path string
+	if noRoot {
+		path = filepath.Join("internal", "print", "print_test.go")
+	} else {
+		path = filepath.Join(name, "internal", "print", "print_test.go")
+	}
+	data := `// Package print defines functions to accept colored standard output and user input
+package print
+
+import (
+	"bytes"
+	"errors"
+	"io"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestInfo(t *testing.T) {
+	type args struct {
+		msg string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Print message",
+			args: args{
+				msg: "test message",
+			},
+			want: []string{"INFO : test message", ""},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			orgStdout := Stdout
+			orgStderr := Stderr
+			pr, pw, err := os.Pipe()
+			if err != nil {
+				t.Fatal(err)
+			}
+			Stdout = pw
+			Stderr = pw
+
+			Info(tt.args.msg)
+			pw.Close()
+			Stdout = orgStdout
+			Stderr = orgStderr
+
+			if err != nil {
+				return
+			}
+
+			buf := bytes.Buffer{}
+			_, err = io.Copy(&buf, pr)
+			if err != nil {
+				t.Error(err)
+			}
+			got := strings.Split(buf.String(), "\n")
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("value is mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestWarn(t *testing.T) {
+	type args struct {
+		msg string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Print message",
+			args: args{
+				msg: "test message",
+			},
+			want: []string{"WARN : test message", ""},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			orgStdout := Stdout
+			orgStderr := Stderr
+			pr, pw, err := os.Pipe()
+			if err != nil {
+				t.Fatal(err)
+			}
+			Stdout = pw
+			Stderr = pw
+
+			Warn(tt.args.msg)
+			pw.Close()
+			Stdout = orgStdout
+			Stderr = orgStderr
+
+			if err != nil {
+				return
+			}
+
+			buf := bytes.Buffer{}
+			_, err = io.Copy(&buf, pr)
+			if err != nil {
+				t.Error(err)
+			}
+			got := strings.Split(buf.String(), "\n")
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("value is mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestErr(t *testing.T) {
+	type args struct {
+		msg string
+	}
+	tests := []struct {
+		name string
+		args args
+		want []string
+	}{
+		{
+			name: "Print message",
+			args: args{
+				msg: "test message",
+			},
+			want: []string{"ERROR: test message", ""},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			orgStdout := Stdout
+			orgStderr := Stderr
+			pr, pw, err := os.Pipe()
+			if err != nil {
+				t.Fatal(err)
+			}
+			Stdout = pw
+			Stderr = pw
+
+			Err(tt.args.msg)
+			pw.Close()
+			Stdout = orgStdout
+			Stderr = orgStderr
+
+			if err != nil {
+				return
+			}
+
+			buf := bytes.Buffer{}
+			_, err = io.Copy(&buf, pr)
+			if err != nil {
+				t.Error(err)
+			}
+			got := strings.Split(buf.String(), "\n")
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("value is mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestFatal(t *testing.T) {
+	type args struct {
+		msg string
+	}
+	tests := []struct {
+		name     string
+		args     args
+		want     []string
+		exitcode int
+	}{
+		{
+			name: "Print message",
+			args: args{
+				msg: "test message",
+			},
+			want:     []string{"FATAL: test message", ""},
+			exitcode: 1,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			orgStdout := Stdout
+			orgStderr := Stderr
+			pr, pw, err := os.Pipe()
+			if err != nil {
+				t.Fatal(err)
+			}
+			Stdout = pw
+			Stderr = pw
+
+			orgOsExit := OsExit
+			exitCode := 0
+			OsExit = func(code int) {
+				exitCode = code
+			}
+			defer func() { OsExit = orgOsExit }()
+
+			Fatal(tt.args.msg)
+			pw.Close()
+			Stdout = orgStdout
+			Stderr = orgStderr
+
+			if err != nil {
+				return
+			}
+
+			buf := bytes.Buffer{}
+			_, err = io.Copy(&buf, pr)
+			if err != nil {
+				t.Error(err)
+			}
+			got := strings.Split(buf.String(), "\n")
+
+			if diff := cmp.Diff(tt.want, got); diff != "" {
+				t.Errorf("value is mismatch (-want +got):\n%s", diff)
+			}
+
+			if exitCode != tt.exitcode {
+				t.Errorf("value is mismatch. want=%d got=%d", exitCode, tt.exitcode)
+			}
+		})
+	}
+}
+
+func TestQuestion(t *testing.T) {
+	type args struct {
+		ask string
+	}
+	tests := []struct {
+		name  string
+		args  args
+		input string
+		want  bool
+	}{
+		{
+			name:  "user input 'y'",
+			args:  args{"no check"},
+			input: "y",
+			want:  true,
+		},
+		{
+			name:  "user input 'yes'",
+			args:  args{"no check"},
+			input: "yes",
+			want:  true,
+		},
+		{
+			name:  "user input 'n'",
+			args:  args{"no check"},
+			input: "n",
+			want:  false,
+		},
+		{
+			name:  "user input 'no'",
+			args:  args{"no check"},
+			input: "no",
+			want:  false,
+		},
+		{
+			name:  "user input 'yes' after 'a'",
+			args:  args{"no check"},
+			input: "a\nyes",
+			want:  true,
+		},
+		{
+			name:  "user only input enter",
+			args:  args{"no check"},
+			input: "\nyes",
+			want:  true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			funcDefer, err := mockStdin(t, tt.input)
+			if err != nil {
+				t.Fatal(err)
+			}
+			defer funcDefer()
+
+			if got := Question(tt.args.ask); got != tt.want {
+				t.Errorf("Question() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestQuestion_FmtScanlnErr(t *testing.T) {
+	t.Run("fmt.Scanln() return error", func(t *testing.T) {
+		orgFmtScanln := FmtScanln
+		FmtScanln = func(a ...any) (n int, err error) {
+			return -1, errors.New("some error")
+		}
+		defer func() { FmtScanln = orgFmtScanln }()
+
+		if got := Question("no check"); got != false {
+			t.Errorf("Question() = %v, want %v", got, false)
+		}
+	})
+}
+
+// mockStdin is a helper function that lets the test pretend dummyInput as os.Stdin.
+// It will return a function for defer to clean up after the test.
+func mockStdin(t *testing.T, dummyInput string) (funcDefer func(), err error) {
+	t.Helper()
+
+	oldOsStdin := os.Stdin
+	tmpFile, err := os.CreateTemp(t.TempDir(), "morrigan_")
+
+	if err != nil {
+		return nil, err
+	}
+
+	content := []byte(dummyInput)
+
+	if _, err := tmpFile.Write(content); err != nil {
+		return nil, err
+	}
+
+	if _, err := tmpFile.Seek(0, 0); err != nil {
+		return nil, err
+	}
+
+	// Set stdin to the temp file
+	os.Stdin = tmpFile
+
+	return func() {
+		// clean up
+		os.Stdin = oldOsStdin
+		os.Remove(tmpFile.Name())
+	}, nil
+}
+`
 	return path, data
 }
 
